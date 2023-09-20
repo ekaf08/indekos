91c652b61a4282d5be6a346cbfda6fda38a0dcb0
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2023-09-20 08:57:08 +0700

    menambahkan fungsi getSubMenu untuk validasi url submenu

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\RoleMenu;
+use Illuminate\Support\Facades\Route;
 
 class Roles extends Model
 {
@@ -19,5 +20,22 @@ class Roles extends Model
     public function menu()
     {
         return $this->hasMany(RoleMenu::class, 'id_role')->orderBy('sort', 'asc');
+    }
+
+    static public function getSubMenu()
+    {
+        $url = Route::currentRouteName();
+        $getMenu = Roles::withTrashed()
+            ->leftJoin('role_menu', 'role_menu.id_role', 'roles.id')
+            ->leftJoin('role_sub_menu', 'role_sub_menu.id_role_menu', 'role_menu.id')
+            ->leftJoin('sub_menu', 'sub_menu.id', 'role_sub_menu.id_sub_menu')
+            ->whereNotNull('role_sub_menu.deleted_at')
+            ->where('roles.id', auth()->user()->id_role)
+            ->where('sub_menu.url', $url)
+            ->select('role_sub_menu.*', 'sub_menu.url')
+            ->withTrashed()
+            ->first();
+
+        return $getMenu;
     }
 }
